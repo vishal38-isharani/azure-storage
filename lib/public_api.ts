acb99c3eda9d7499f4f24030d524e479d1b32d40
Vishal isharani <isharanivishal@gmail.com>
2020-08-23 19:12:57 +0530

    module resolution fixes

--- lib/public_api.ts
+++ lib/public_api.ts
@@ -2,3 +2,4 @@ export * from './azure-storage-file.interceptor';
 export * from './azure-storage.constant';
 export * from './azure-storage.service';
 export * from './azure-nest-storage.module';
+export * from './azure-nest-storage.interface';
